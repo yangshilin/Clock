d5bb7ff75f3a0cd16cf2c12ee23366859946baf0
yuan13827 <1747158566@qq.com>
2016-08-15 15:52:51 +0800

    局部小改动

--- Xu_Clock/src/com/example/adapter/MyAdapterFindLuyin.java
+++ Xu_Clock/src/com/example/adapter/MyAdapterFindLuyin.java
@@ -10,7 +10,7 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.example.alarmclok.view.FindSetgetLuyin;
-import com.example.clock.R;
+
 
 public class MyAdapterFindLuyin extends BaseAdapter{
 	LayoutInflater inflater;//���������
